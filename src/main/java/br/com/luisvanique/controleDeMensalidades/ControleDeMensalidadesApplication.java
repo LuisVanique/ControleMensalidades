0d6c949976efe2ea00d8cda1b39f6ba806a1ce4b
LuisVanique <cunhagustavo142@gmail.com>
2025-01-23 07:09:34 -0300

    primeiro commit

---
+++ src/main/java/br/com/luisvanique/controleDeMensalidades/ControleDeMensalidadesApplication.java
@@ -0,0 +1,13 @@
+package br.com.luisvanique.controleDeMensalidades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeMensalidadesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeMensalidadesApplication.class, args);
+	}
+
+}
