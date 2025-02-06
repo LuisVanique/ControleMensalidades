4569de9fa63c3acf08fb6f2b81ccebb613f7cec1
LuisVanique <cunhagustavo142@gmail.com>
2025-02-06 13:36:49 -0300

    alteracao de controller para records

--- src/main/java/br/com/luisvanique/controleDeMensalidades/ControleDeMensalidadesApplication.java
+++ src/main/java/br/com/luisvanique/controleDeMensalidades/ControleDeMensalidadesApplication.java
@@ -3,7 +3,7 @@ package br.com.luisvanique.controleDeMensalidades;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "br.com.luisvanique.controleDeMensalidades")
+@SpringBootApplication
 public class ControleDeMensalidadesApplication {
 
 	public static void main(String[] args) {
