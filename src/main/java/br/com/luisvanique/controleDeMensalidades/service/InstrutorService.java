14613149f6323408222388419718f4661530e458
LuisVanique <cunhagustavo142@gmail.com>
2025-02-12 10:12:42 -0300

    Adiciona endpoint de find all e find id instrutor

--- src/main/java/br/com/luisvanique/controleDeMensalidades/service/InstrutorService.java
+++ src/main/java/br/com/luisvanique/controleDeMensalidades/service/InstrutorService.java
@@ -3,11 +3,14 @@ package br.com.luisvanique.controleDeMensalidades.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import br.com.luisvanique.controleDeMensalidades.dto.InstrutorDto;
+import br.com.luisvanique.controleDeMensalidades.exception.ObjectNotFoundException;
 import br.com.luisvanique.controleDeMensalidades.model.Instrutor;
 import br.com.luisvanique.controleDeMensalidades.repository.InstrutorRepository;
 import br.com.luisvanique.controleDeMensalidades.validation.ICreateInstrutorValidator;
@@ -33,5 +36,14 @@ public class InstrutorService {
 		repository.save(instrutor);
 		return instrutor;
 	}
+	
+	public Page<Instrutor> findAll(Pageable pageable) {
+		return repository.findAll(pageable);
+	}
+	
+	public Instrutor findById(Long id) {
+		return repository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Instrutor não encontrado"));
+	}
+
 	
 }
